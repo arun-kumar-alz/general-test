12604d9bb25ec1eae36ad7a01fde0cb3d5e26b4b
Arunkumar Thulasidharan <arun.kumar@ad.wyfl.allianzuk.cloud>
2021-02-14 12:20:46 +0530

    Arun error code

--- src/main/java/arun/snyk/test/route/GreetingController.java
+++ src/main/java/arun/snyk/test/route/GreetingController.java
@@ -3,6 +3,8 @@ package arun.snyk.test.route;
 import java.util.concurrent.atomic.AtomicLong;
 
 import arun.snyk.test.model.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,11 @@ public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	private Logger log = LoggerFactory.getLogger(GreetingController.class);
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		log.info("User input data "+name);
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
